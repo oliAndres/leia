178b8c7d819d37fe36950f06f1a4818993857116
andrés <olivera.h.andres@gmail.com>
2023-07-01 06:21:37 -0300

    Crea estructura basica HTML

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000
+
+app.use(express.static('public'));
+
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
+app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
+
+
